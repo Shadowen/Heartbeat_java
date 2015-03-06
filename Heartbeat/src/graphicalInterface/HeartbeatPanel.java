cb90421af8ea12868421bc321672b6145be217ac
wesleyOnLucy <wesley@heungs.com>
2015-03-06 01:36:50 -0500

    Added a bit of automation to the graph in HeartbeatPanel.

--- Heartbeat/src/graphicalInterface/HeartbeatPanel.java
+++ Heartbeat/src/graphicalInterface/HeartbeatPanel.java
@@ -14,7 +14,7 @@ import javax.swing.JPanel;
 public class HeartbeatPanel extends JPanel {
 	private static final int NUM_PREVIOUS_TIMES = 100;
 	/** Scaling factor to convert milliseconds to pixels on the graph **/
-	private static final double TIME_SCALING = 0.20;
+	private double timeScaling = 0.20;
 	/** Y-coordinate of the bottom of the graph **/
 	private static final double GRAPH_BOTTOM = 300;
 
@@ -36,32 +36,37 @@ public class HeartbeatPanel extends JPanel {
 		long currentTime = System.currentTimeMillis();
 		previousTimes.add(currentTime - lastHeartbeat);
 		lastHeartbeat = currentTime;
+
+		// Maintain maximum size of buffer
 		if (previousTimes.size() > NUM_PREVIOUS_TIMES) {
 			previousTimes.poll();
 		}
+
+		// Increase the scale if necessary
+		while (previousTimes.peek() * timeScaling > GRAPH_BOTTOM) {
+			timeScaling /= 2;
+		}
+
+		// Request repaint
 		repaint();
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+
 		// Draw axis
 		g.setColor(Color.BLUE);
-		// 100
-		g.drawLine(previousTimes.size() * graphHorizontalSpacing,
-				(int) (GRAPH_BOTTOM - 100 * TIME_SCALING), previousTimes.size()
-						* graphHorizontalSpacing + 10,
-				(int) (GRAPH_BOTTOM - 100 * TIME_SCALING));
-		g.drawString("100", previousTimes.size() * graphHorizontalSpacing + 20,
-				(int) (GRAPH_BOTTOM - (100 * TIME_SCALING - 5)));
-		// 1000
-		g.drawLine(previousTimes.size() * graphHorizontalSpacing,
-				(int) (GRAPH_BOTTOM - 1000 * TIME_SCALING),
-				previousTimes.size() * graphHorizontalSpacing + 10,
-				(int) (GRAPH_BOTTOM - 1000 * TIME_SCALING));
-		g.drawString("1000",
-				previousTimes.size() * graphHorizontalSpacing + 20,
-				(int) (GRAPH_BOTTOM - (1000 * TIME_SCALING - 5)));
+		for (int i = 1; i < GRAPH_BOTTOM / timeScaling; i *= 10) {
+			// 100
+			g.drawLine(previousTimes.size() * graphHorizontalSpacing,
+					(int) (GRAPH_BOTTOM - i * timeScaling),
+					previousTimes.size() * graphHorizontalSpacing + 10,
+					(int) (GRAPH_BOTTOM - i * timeScaling));
+			g.drawString(String.valueOf(i), previousTimes.size()
+					* graphHorizontalSpacing + 20, (int) (GRAPH_BOTTOM - (i
+					* timeScaling - 5)));
+		}
 
 		// Draw points
 		g.setColor(Color.RED);
@@ -71,9 +76,9 @@ public class HeartbeatPanel extends JPanel {
 		while (it.hasNext()) {
 			long thisTime = it.next();
 			g.drawLine(i * graphHorizontalSpacing,
-					(int) (GRAPH_BOTTOM - lastTime * TIME_SCALING), (i + 1)
+					(int) (GRAPH_BOTTOM - lastTime * timeScaling), (i + 1)
 							* graphHorizontalSpacing,
-					(int) (GRAPH_BOTTOM - thisTime * TIME_SCALING));
+					(int) (GRAPH_BOTTOM - thisTime * timeScaling));
 			lastTime = thisTime;
 			i++;
 		}
